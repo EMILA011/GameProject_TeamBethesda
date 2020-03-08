ed8943311d1429463ef456c90c8a4f882c2a3b5d
Elena <emilanlopez@bellarmine.edu>
2020-03-08 10:25:52 -0400

    player updated

---
+++ Assets/camerascript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Camera follows the player around the screen
+
+public class camerascript : MonoBehaviour
+{
+     //object that the camera follows
+     public Transform followPlayer;
+
+     //modify the position
+     private Vector3 moveTemp;
+     public float offsetX = 3;
+     public float offsetY = 3;
+
+     // Use this for initialization
+     void Start()
+     {
+          
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+          // Player position.
+          moveTemp = followPlayer.transform.position;
+
+          //offset x-coordinate 3 units
+          moveTemp.x += offsetX;
+
+          //offset y-coordinate 3 units
+          moveTemp.y += offsetY;
+
+          //modify z-coordinate to be the same as the main camera z-coordinate.
+          moveTemp.z = transform.position.z;
+
+          //asing main camera position to wherever the Player is located.
+          transform.position = moveTemp;
+     }
+}
